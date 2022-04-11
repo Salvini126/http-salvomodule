f5a1186f969c1074f3f714357a5ec2a260857d8f
Salvini126 <salvini.matteo@iisgalvanimi.edu.it>
2022-04-11 07:48:48 +0000

    punto finale

--- salvo-http/src/app/foo/foo.component.spec.ts
+++ salvo-http/src/app/foo/foo.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FooComponentComponent } from './foo-component.component';
+import { FooComponentComponent } from './foo.component';
 
 describe('FooComponentComponent', () => {
   let component: FooComponentComponent;
